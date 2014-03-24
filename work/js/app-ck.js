5a7843eec77a823b21e01de6f74d505b18e0f7d9
Philipp Grynaeus <github@grynaeus.de>
2014-03-24 22:06:56 +0100

    Teil2
    new code added / CSS and Images

---
+++ work/js/app-ck.js
@@ -0,0 +1,1 @@
+$(function(){$(".menus h3").on("click",function(e){$(this).next("ul").toggleClass("open");e.preventDefault();return!1})});
--- work/js/app.js
+++ work/js/app.js
@@ -1,1 +1,8 @@
-<!-- JS -->
+$(function() {
+ 
+    $('.menus h3').on('click', function(e) {
+        $(this).next('ul').toggleClass('open');
+        e.preventDefault(); return false;
+    });
+ 
+});
